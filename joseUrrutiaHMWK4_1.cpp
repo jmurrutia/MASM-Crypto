59e09d8b25008a6380deb573619cd77c3dc6459c
jmurrutia <32080154+jmurrutia@users.noreply.github.com>
2017-12-05 00:54:52 -0800

    Initial Commit

--- joseUrrutiaHMWK4_1.cpp
+++ joseUrrutiaHMWK4_1.cpp
@@ -1,13 +1,3 @@
-/*
-;     Jose M. Urrutia
-;     November 3, 2016
-;     Visual Studio 2015
-;     TITLE Text Encrption Crypto Program
-;     This program uses C++ and Masm in mixed mode to take
-;     an inputed string of text, encrypts the text and displays it, 
-;     then decrypts the text and displays the original text.
-*/
-
 #include <iostream>
 #include <string>
 #include <cstring>
